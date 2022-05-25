e8aaa7e81391f902b3d3faca5ce5d71a2586c0e5
vivek-cd <69166772+vivek-cd@users.noreply.github.com>
2022-05-25 17:37:13 +0530

    2nd update

--- app.py
+++ app.py
@@ -1,54 +1,30 @@
-#from types import NoneType
-import numpy as np
-import pandas as pd
+# !pip install transformers==2.8.0
+# !pip install torch==1.4.0
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 from flask import Flask, request, render_template
-#from sklearn.externals 
-import joblib
 
 app = Flask(__name__)
-
-
-def loadm():
-    file= open("model_joblib.pkl","rb")
-    model = joblib.load(file,mmap_mode=None)
-    return model
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    model = loadm()
-    # input_features = [float(x) for x in request.form.values()]
-    # features_value = [np.array(input_features)]
-    
-    # features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
-    #    'mean smoothness', 'mean compactness', 'mean concavity',
-    #    'mean concave points', 'mean symmetry', 'mean fractal dimension',
-    #    'radius error', 'texture error', 'perimeter error', 'area error',
-    #    'smoothness error', 'compactness error', 'concavity error',
-    #    'concave points error', 'symmetry error', 'fractal dimension error',
-    #    'worst radius', 'worst texture', 'worst perimeter', 'worst area',
-    #    'worst smoothness', 'worst compactness', 'worst concavity',
-    #    'worst concave points', 'worst symmetry', 'worst fractal dimension']
-    
-    #df = pd.DataFrame(features_value, columns=features_name)
-    output = model.predict(request.form.values())
-        
-    # if output == 0:
-    #     res_val = "** breast cancer **"
-    # else:
-    #     res_val = "no breast cancer" .format(output)
-        
-
-
-    return render_template('index.html', prediction_text= output )
+    model = T5ForConditionalGeneration.from_pretrained('t5-small')
+    tokenizer = T5Tokenizer.from_pretrained('t5-small')
+    device = torch.device('cpu')
+    text = request.form.values()
+    preprocessed_text = text.strip().replace('\n','')
+    t5_input_text =  preprocessed_text
+    tokenized_text = tokenizer.encode(t5_input_text, return_tensors='pt', max_length=512).to(device)
+    summary_ids = model.generate(tokenized_text, min_length=30, max_length=120)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return render_template('index.html', prediction_text= summary )
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-    
     
     
     
